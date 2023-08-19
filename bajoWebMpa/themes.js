5191774ff1b705ad22dafad3e654a518c0bbc215
Ardhi Lukianto <ardhi@lukianto.com>
2023-08-19 10:05:18 +0700

    Helper renamed

--- bajoWebMpa/themes.js
+++ bajoWebMpa/themes.js
@@ -1,15 +1,15 @@
 async function themes () {
-  const { assetsDir } = this.bajoWebStatic.helper
+  const { assetDir } = this.bajoWebStatic.helper
   return [{
     name: 'adminlte3',
     framework: 'bootstrap4',
     description: 'Admin LTE 3',
     scriptMerge: true,
     script: [
-      `${assetsDir('bajoWebMpaAdminlte')}/adminlte-3.2/js/adminlte.min.js`
+      `${assetDir('bajoWebMpaAdminlte')}/adminlte-3.2/js/adminlte.min.js`
     ],
     css: [
-      `${assetsDir('bajoWebMpaAdminlte')}/adminlte-3.2/css/adminlte.min.css`
+      `${assetDir('bajoWebMpaAdminlte')}/adminlte-3.2/css/adminlte.min.css`
     ]
   }]
 }
